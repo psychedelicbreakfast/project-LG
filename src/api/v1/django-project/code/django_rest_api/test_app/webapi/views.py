2211f5496b8056e37370c68419f1b805f49ea324
psychedelicbreakfast <picturesofacity@outlook.jp>
2021-07-01 02:10:03 +0900

    build: DRF,Postgre環境を再構築

--- src/api/v1/django-project/code/django_rest_api/test_app/webapi/views.py
+++
@@ -1,6 +0,0 @@
-from webapi.models import Profile
-from webapi.serializers import ProfileSerializer
-from rest_framework import generics
-class ProfileListCreate(generics.ListCreateAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
